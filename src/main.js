68b3f099e87cfe12d1d68c74edd8ca2b546de7d3
Pavel Rubis <Pavel.Rubis@comindware.ru>
2024-05-11 12:51:22 +0300

    default path added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Archiver } from "./Archiver.js";
 import { NextCloudUploader } from "./NextCloudUploader.js";
 
 const archiver = new Archiver();
-await archiver.archiveDirectory("D:/Education/GoF/UML", "n8ndata");
+await archiver.archiveDirectory("/home/<username>/n8n_backups/n8n_data/", "n8ndata");
 console.log("archiver result " + archiver.archivedSuccess);
 
 const uploader = new NextCloudUploader();
